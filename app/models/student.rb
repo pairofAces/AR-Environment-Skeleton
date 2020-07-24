0639dacace7089e60c013ccaebc948db49727318
pairofAces <karanchauhan16@gmail.com>
2020-07-24 10:37:42 -0400

    part 4

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 class Student < ActiveRecord::Base
     has_many :grade_levels
-    has_many :teachers, through: :grade_levels
+    has_many :lectures, through: :grade_levels
+    
+    
+    # has_many :grade_levels
+    # has_many :teachers, through: :grade_levels
 
     def full_name
         "#{self.first_name} #{self.last_name}"
